4bc6a93ef6a5f720c8dbc081079ef46d4d79d909
Beat Reichenbach <44111292+beatreichenbach@users.noreply.github.com>
2026-09-17 16:52:53 -0700

    build: revert github action

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-rm -rf build
-mkdir -p build/megascans build/python
-cp src/*.json build/
-cp -r src/menu build/megascans/
-python -m pip install . --target build/python
-cd build
-zip -r megascans-$NEW_VERSION.zip .
